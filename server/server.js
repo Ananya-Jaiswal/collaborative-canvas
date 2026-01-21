9d38fc8107b0357c4cb72563c1e1476465bd5c86
Ananya Jaiswal <ananyajaiswal62@gmail.com>
2026-01-21 17:57:54 +0530

    Fix SPA fallback routing for Node 22 / Express

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "../client")));
 
 // IMPORTANT: Express-safe wildcard
-app.get("/*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../client/index.html"));
 });
 
